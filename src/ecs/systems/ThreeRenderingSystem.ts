da845e5584c39776080d2965096dfcd6ca8f7d65
Paulo Ragonha <paulo@ragonha.me>
2020-08-11 15:48:49 +0200

    Convert the positions to 3D space 🤯

--- src/ecs/systems/ThreeRenderingSystem.ts
+++ src/ecs/systems/ThreeRenderingSystem.ts
@@ -7,36 +7,26 @@ import RenderableComponent from '../components/RenderableComponent'
 export default class ThreeRenderingSystem extends System {
   scene!: THREE.Scene
   renderer!: THREE.WebGLRenderer
-  camera!: THREE.OrthographicCamera
+  camera!: THREE.Camera
 
   init() {
     const scene = new THREE.Scene()
+    this.scene = scene
 
     const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.setClearColor(0x1b363e)
+    this.renderer = renderer
 
-    const camera = new THREE.OrthographicCamera(
-      window.innerWidth / -10,
-      window.innerWidth / 10,
-      window.innerHeight / 10,
-      window.innerHeight / -10,
-      1,
-      100,
-    )
-    camera.position.x = 0
-    camera.position.y = 2
-    camera.position.z = 2
-    camera.lookAt(new THREE.Vector3(0, 0, 2))
-
+    const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.y = 40
+    camera.position.z = 40
+    camera.lookAt(0, 0, 0)
     scene.add(camera)
+    this.camera = camera
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
     scene.add(ambientLight)
-
-    this.scene = scene
-    this.renderer = renderer
-    this.camera = camera
 
     document.body.appendChild(renderer.domElement)
   }
@@ -49,7 +39,7 @@ export default class ThreeRenderingSystem extends System {
     const geometry = new THREE.SphereBufferGeometry()
     const material = new THREE.MeshStandardMaterial({ color: renderable.color })
     const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.set(position.x / 10 - 40, 0, position.y / 10 - 40)
+    mesh.position.copy(convertPosition(position))
     this.scene.add(mesh)
 
     entity.addComponent(ThreeRenderingSystemStateComponent, { mesh })
@@ -60,7 +50,7 @@ export default class ThreeRenderingSystem extends System {
     const position = entity.getComponent(PositionComponent)
     if (!position || !stateComponent || !stateComponent.mesh) return
 
-    stateComponent.mesh.position.set(position.x / 10 - 40, 0, position.y / 10 - 40)
+    stateComponent.mesh.position.copy(convertPosition(position))
   }
 
   removeRenderable(entity: Entity) {
@@ -98,4 +88,19 @@ export default class ThreeRenderingSystem extends System {
       listen: { removed: true },
     },
   }
+}
+
+/**
+ * Converts our positions that are based on the screen size to something that we can
+ * use in 3D.
+ *
+ * Also the vertical axis in Three.js is actually the `z`
+ */
+const convertPosition = (position: PositionComponent) => {
+  const canvas = 80
+
+  const x = (position.x / window.innerWidth) * canvas - canvas / 2
+  const y = 0
+  const z = (position.y / window.innerHeight) * canvas - canvas / 2
+  return new THREE.Vector3(x, y, z)
 }
